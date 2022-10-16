18a7ea9dc3685b96fe64a155034179552a2502a7
Safi <safikurrahman7514@gmail.com>
2022-10-16 21:22:24 +0600

    start random background color flipper

---
+++ app.js
@@ -0,0 +1,7 @@
+const colors = ['red', 'green', 'rgb(18,14,67)', 'rgb(70,178,230)'];
+
+// Selected btn and color element from document object
+const btn = document.querySelector('#btn');
+const color = document.querySelector('.color');
+
+// Event listener handle on btn here
