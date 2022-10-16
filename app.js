be620ec28bd11bd78e62fbd93a4c7ce2bba4a840
Safi <safikurrahman7514@gmail.com>
2022-10-16 21:42:23 +0600

    done random background colors

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const colors = ['red', 'green', 'rgb(18,14,67)', 'rgb(70,178,230)'];
-
+// console.log(colors[1]);
 // Selected btn and color element from document object
 const btn = document.querySelector('#btn');
 const color = document.querySelector('.color');
 
 // Event listener handle on btn here
+btn.addEventListener('click', function () {
+    const randomColor = getRandomColors();
+    console.log(randomColor);
+    document.body.style.backgroundColor = colors[randomColor];
+    color.textContent = colors[randomColor];
+});
+
+// Get random function 
+
+function getRandomColors() {
+    return Math.floor(Math.random() * colors.length);
+}
